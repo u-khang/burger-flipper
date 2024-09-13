74cb8658a6e59f468405bfcbc551b8997ac71bfa
u-khang <khang.nguyen8675@gmail.com>
2024-09-13 15:34:45 -0400

    register POST logic

--- whopper/auth.py
+++ whopper/auth.py
@@ -9,4 +9,29 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
-    return "regista"
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+    
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+    
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password))
+                )
+                db.commit()
+            except db.IntegrityError:
+                return f"Username {username} is already registered."
+            else:
+                return "login"
+
+        flash(error)
+
+    return "register"
