0a00f3586c5aa25449658e74c2d4c5c3f0e45fb1
u-khang <khang.nguyen8675@gmail.com>
2024-09-21 13:35:43 -0400

    user info available to views

--- whopper/ticket.py
+++ whopper/ticket.py
@@ -10,20 +10,19 @@ def ticket():
         n1 = request.form['n1']
         n2 = request.form['n2']
         n3 = request.form['n3']
-        # db = get_db()
-        # error = None
+        db = get_db()
+        error = None
 
-        # print(session['user_id'])
-        # print(type(n1))
-        # print(f"{n1}{n2}{n3}")
+        print(session['user_id'])
+        print(type(n1))
+        print(f"{n1}{n2}{n3}")
 
-        # if error is None:
-        #     print(session['user_id'])
-        #     db.execute(
-        #         "INSERT INTO ticket (user_id, numbers) VALUES (?, ?)",
-        #         (session['user_id'], f"{n1}{n2}{n3}")
-        #     )
-        #     db.commit()
+        if error is None:
+            db.execute(
+                "INSERT INTO ticket (user_id, numbers) VALUES (?, ?)",
+                (session['user_id'], f"{n1}{n2}{n3}")
+            )
+            db.commit()
         return redirect(url_for('ticket.ticket'))
 
     return render_template('ticket.html')
