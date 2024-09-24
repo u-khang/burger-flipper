447d0423430250d9c14a82355bb858d91592ee56
u-khang <khang.nguyen8675@gmail.com>
2024-09-24 17:20:43 -0400

    query user_tickets

--- whopper/user.py
+++ whopper/user.py
@@ -1,18 +1,26 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+from whopper.db import get_db
 
 user_bp = Blueprint('user', __name__)
 @user_bp.route('/user', methods=('GET', 'POST'))
 def user():
+    db = get_db()
+
+    try:
+        user_tickets = db.execute(
+            'SELECT * FROM ticket WHERE user_id = ?', (str(g.user[0]))
+        ).fetchall()
+    except:
+        return 'uh oh something happened'
+    print(user_tickets)
+
     if request.method == 'POST':
-
         error = None
-
         if error is None:
             return ''
-
         print(error)
         flash(error)
 
-    return render_template('user.html')
+    return render_template('user.html', user_tickets = user_tickets)
