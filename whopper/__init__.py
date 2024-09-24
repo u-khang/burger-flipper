57a7916439b0cd1e863ea3d4d831bb0b0325ccd2
u-khang <khang.nguyen8675@gmail.com>
2024-09-24 02:52:23 -0400

    init-db doesnt work

--- whopper/__init__.py
+++ whopper/__init__.py
@@ -1,20 +1,52 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, current_app, g
 from . import db
-from whopper.ticket import generate_winning_nums
+
 
 app = Flask(__name__)
 app.config.from_mapping(
     SECRET_KEY='manilovefauna',
     DATABASE=os.path.join(app.instance_path, 'whopper.sqlite'),
 )
+
 db.init_app(app)
 from . import auth, ticket, main
 app.register_blueprint(auth.auth_bp)
 app.register_blueprint(ticket.ticket_bp)
 app.register_blueprint(main.main_bp)
 
-generate_winning_nums()
+
+from whopper.ticket import generate_winning_nums
+import threading, time
+from datetime import datetime
+
+
+# def drawing_loop():
+#     while True:
+#         with app.app_context():
+#             winning_nums = generate_winning_nums()
+#             print(winning_nums[0])
+#             my_db = db.get_db()
+
+#             error = None
+
+#             if error is None:
+#                 my_db.execute(
+#                     "INSERT INTO drawing (draw_date, winning_nums) VALUES (?, ?)",
+#                     winning_nums
+#                 )
+#                 my_db.commit()
+
+#             print(error)
+#             # flash(error)
+
+
+#             time.sleep(5)
+
+# thread = threading.Thread(target=drawing_loop)
+# thread.daemon = True
+# thread.start()
+
 
 @app.route("/")
 def index():
--- whopper/main.py
+++ whopper/main.py
@@ -1,11 +1,42 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from whopper.db import get_db
+# from whopper.db import get_db
+
+from whopper.ticket import generate_winning_nums
+import threading, time
+from datetime import datetime
+
+
+# def drawing_loop():
+#     while True:
+#         winning_nums = generate_winning_nums()
+#         print(winning_nums[0])
+#         # db = get_db()
+#         # error = None
+
+#         # if error is None:
+#         #     db.execute(
+#         #         "INSERT INTO drawing (user_id, numbers) VALUES (?, ?)",
+#         #         (session['user_id'], f"{n1}{n2}{n3}")
+#         #     )
+#         #     db.commit()
+#         #     return redirect(url_for('ticket.ticket'))
+
+#         # print(error)
+#         # flash(error)
+
+
+#         time.sleep(5)
+
+# thread = threading.Thread(target=drawing_loop)
+# thread.daemon = True
+# thread.start()
+
+
+
 
 main_bp = Blueprint('main', __name__)
 @main_bp.route('/', methods=('GET', 'POST'))
 def main():
-    winning_nums = (7,3,0)
-    g.winning_nums = winning_nums
     return render_template('index.html')
